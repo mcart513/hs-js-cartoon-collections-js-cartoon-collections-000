edf13038a0a36553a39698288f1d3b061fe3261f
Michael Carter <mcart513@gmail.com>
2020-08-16 19:25:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function dwarfRollCall(dwarves) {
-  var names = ["Doc", "Dopey", "Bashful", "Grumpy"];
-  return "1. " + names[0] + "2. " + names[1] + "3. " + names[2] + "4. " + names[3];
+  var names = ["Dopey", "Grumpy", "Bashful"];
+  return "1. " + names[0] + " 2. " + names[1] + " 3. " + names[2];
 }
 
 function summonCaptainPlanet(planeteerCalls){
